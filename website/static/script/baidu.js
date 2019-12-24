7d1728c8b69e28d05b10417a13d2997285b488ea
summerscar <shen.yus@163.com>
2019-12-24 11:58:06 +0800

    增加百度商桥

---
+++ website/static/script/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8e2dea80f174b0f8482cd37cb9843c8d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
